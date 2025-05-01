d3f583e0e0feb8b2d465266c57239f1cd15471f7
yuukka <yuukinba0105@gmail.com>
2025-05-01 23:41:55 +0700

    Ver1.1 fixed Drag and Drop Bug

--- js/data.js
+++ js/data.js
@@ -1,18 +1,10 @@
 export const quizzes = [
-  { number: 1, show: false, name: 'くるま', wordCount: 3, photo: "https://images.pexels.com/photos/18639194/pexels-photo-18639194/free-photo-of-vintage-yellow-car-with-eyes-and-advertisement.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { number: 2, show: false, name: 'ぱとかー', wordCount: 4, photo: "https://media.istockphoto.com/id/860991168/ja/%E3%83%99%E3%82%AF%E3%82%BF%E3%83%BC/%E7%8F%BE%E4%BB%A3%E3%81%AE%E9%83%BD%E5%B8%82%E8%AD%A6%E5%AF%9F%E3%83%91%E3%83%88%E3%83%AD%E3%83%BC%E3%83%AB%E8%BB%8A%E4%B8%A1%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88.jpg?s=1024x1024&w=is&k=20&c=G6xfntXAWF04F1ni8HtGgY23PUd5gyHbSS9Y9PrqsGw="},
-  { number: 3, show: false, name: 'しょうぼうしゃ', wordCount: 7, photo: "https://as2.ftcdn.net/v2/jpg/03/01/40/01/1000_F_301400138_Xse8uaaYP0WsyPinGK5mFnI4xbsr9tvW.jpg"},
-  { number: 4, show: false, name: 'きゅうきゅうしゃ', wordCount: 8, photo: "https://as1.ftcdn.net/v2/jpg/07/00/10/72/1000_F_700107239_e0OPzuabjXhoV7DGtmyf8TO4PPj11lTQ.webp"},
-  { number: 5, show: false, name: 'じてんしゃ', wordCount: 5, photo: "https://as2.ftcdn.net/jpg/06/23/78/51/1000_F_623785169_rfcSDYjGrvxnkK9eg52OsU6G0m1juC21.jpg"}
+  { number: 1, show: false, name: 'く', wordCount: 3, photo: "https://images.pexels.com/photos/18639194/pexels-photo-18639194/free-photo-of-vintage-yellow-car-with-eyes-and-advertisement.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+  { number: 2, show: false, name: 'ぱ', wordCount: 4, photo: "https://media.istockphoto.com/id/860991168/ja/%E3%83%99%E3%82%AF%E3%82%BF%E3%83%BC/%E7%8F%BE%E4%BB%A3%E3%81%AE%E9%83%BD%E5%B8%82%E8%AD%A6%E5%AF%9F%E3%83%91%E3%83%88%E3%83%AD%E3%83%BC%E3%83%AB%E8%BB%8A%E4%B8%A1%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88.jpg?s=1024x1024&w=is&k=20&c=G6xfntXAWF04F1ni8HtGgY23PUd5gyHbSS9Y9PrqsGw="},
+  { number: 3, show: false, name: 'し', wordCount: 7, photo: "https://as2.ftcdn.net/v2/jpg/03/01/40/01/1000_F_301400138_Xse8uaaYP0WsyPinGK5mFnI4xbsr9tvW.jpg"},
+  { number: 4, show: false, name: 'き', wordCount: 8, photo: "https://as1.ftcdn.net/v2/jpg/07/00/10/72/1000_F_700107239_e0OPzuabjXhoV7DGtmyf8TO4PPj11lTQ.webp"},
+  { number: 5, show: false, name: 'じ', wordCount: 5, photo: "https://as2.ftcdn.net/jpg/06/23/78/51/1000_F_623785169_rfcSDYjGrvxnkK9eg52OsU6G0m1juC21.jpg"}
 ];
-
-// const quizzes = [
-//   { number: 1, show: false, name: 'く', wordCount: 3, photo: "https://images.pexels.com/photos/18639194/pexels-photo-18639194/free-photo-of-vintage-yellow-car-with-eyes-and-advertisement.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-//   { number: 2, show: false, name: 'ぱ', wordCount: 4, photo: "https://media.istockphoto.com/id/860991168/ja/%E3%83%99%E3%82%AF%E3%82%BF%E3%83%BC/%E7%8F%BE%E4%BB%A3%E3%81%AE%E9%83%BD%E5%B8%82%E8%AD%A6%E5%AF%9F%E3%83%91%E3%83%88%E3%83%AD%E3%83%BC%E3%83%AB%E8%BB%8A%E4%B8%A1%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88.jpg?s=1024x1024&w=is&k=20&c=G6xfntXAWF04F1ni8HtGgY23PUd5gyHbSS9Y9PrqsGw="},
-//   { number: 3, show: false, name: 'し', wordCount: 7, photo: "https://as2.ftcdn.net/v2/jpg/03/01/40/01/1000_F_301400138_Xse8uaaYP0WsyPinGK5mFnI4xbsr9tvW.jpg"},
-//   { number: 4, show: false, name: 'き', wordCount: 8, photo: "https://as1.ftcdn.net/v2/jpg/07/00/10/72/1000_F_700107239_e0OPzuabjXhoV7DGtmyf8TO4PPj11lTQ.webp"},
-//   { number: 5, show: false, name: 'じ', wordCount: 5, photo: "https://as2.ftcdn.net/jpg/06/23/78/51/1000_F_623785169_rfcSDYjGrvxnkK9eg52OsU6G0m1juC21.jpg"}
-// ];
 export const hrgn = ["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "や", "ゆ", "よ", "わ", "を", "ん"];
 
 export const states = ["initial-state", "game-state", "win-state", "end-state"];
